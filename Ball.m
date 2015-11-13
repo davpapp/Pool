6f07c91e9ca79e65665cd924c714489ba2c18b79
davpapp <david.papp@students.olin.edu>
2015-11-13 12:02:22 -0500

    Created main.m

--- Ball.m
+++ Ball.m
@@ -1,9 +1,9 @@
 function ball = Ball()
     ball.in_play = true;
     ball.radius = 2.86;
-    ball.mass = 0.170; 
-    ball.v_theta = 0;
-    ball.v_d = 5;
+    ball.mass = 170; 
+    ball.v_theta = pi/2;
+    ball.v_d = 1;
     ball.pos_x = 40;
     ball.pos_y = 50;
 end
--- Main.m
+++ Main.m
@@ -1,7 +1,9 @@
 %main.m
+clf
 ball1 = Ball()
 hold on
 for i=1:100
-    moveBall(ball1);
-    plot(ball1.pos_x, ball1.pos_y);
+    ball1 = moveBall(ball1);
+    plot(ball1.pos_x, ball1.pos_y, 'ro');
+    ball1.v_d
 end
