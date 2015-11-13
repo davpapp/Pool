0eb4bb12ab71ff1ea030984af3b9b69d91bce2fd
JCMathai <john.mathai1@students.olin.edu>
2015-11-13 12:12:10 -0500

    inputted proper x/y terminology

--- checkTableCollision.m
+++ checkTableCollision.m
@@ -1,16 +1,16 @@
-function res = checkTableCollision (Ball)
+function ball = checkTableCollision (ball)
 % Pocket Formation and taking the ball out of play
 
-    if (x < 3.5 || x > 96.5) && (y < 3.5 || y > 46.5)
+    if (ball.pos_x < 3.5 || ball.pos_x > 96.5) && (ball.pos_y < 3.5 || ball.pos_y > 46.5)
         % This if statement defines the inner boundary of the table
-        if (x < 3.5 && (y < 3.5 || y > 46.5) || (x > 96.5 && (y <3.5 || y > 46.5)
+        if (ball.pos_x < 3.5 && (ball.pos_y < 3.5 || ball.pos_y > 46.5) || (ball.pos_x > 96.5 && (ball.pos_y <3.5 || ball.pos_y > 46.5)
             %Checks if the ball is the four corner pockets
             ball.in_play = false;
-        else wallBounce(Ball)
+%         else wallBounce(Ball)
         end
     else ball.in_play = true;
        % Middle pockets
-    if (x < 51.75 && x > 48.25) && (y < 3.5 || y > 46.5)
+    if (ball.pos_x < 51.75 && ball.pos_x > 48.25) && (ball.pos_y < 3.5 || ball.pos_y > 46.5)
         ball.in_play = false;
     else ball.in_play = true;
     end
