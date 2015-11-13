aa484857d0c75d71062de28842f539d09bc22fd0
davpapp <david.papp@students.olin.edu>
2015-11-13 17:56:22 -0500

    Created checkCollision

--- Ball.m
+++ Ball.m
@@ -1,9 +1,9 @@
-function ball = Ball()
+function ball = Ball(x, y, d, theta)
     ball.in_play = true;
     ball.radius = 2.86;
     ball.mass = 170; 
-    ball.v_theta = pi/2;
-    ball.v_d = 1;
-    ball.pos_x = 40;
-    ball.pos_y = 50;
+    ball.v_theta = theta;
+    ball.v_d = d;
+    ball.pos_x = x;
+    ball.pos_y = y;
 end
--- Main.m
+++ Main.m
@@ -1,22 +1,19 @@
 %main.m
 clf
-ball1 = Ball()
-ball2 = Ball()
-ball2.pos_x = 80;
-ball2.pos_y = 70;
-ball2.v_d = 5;
-ball2.v_theta = pi/4;
+ball1 = Ball(50, 0, 6, pi/3.5);
+ball2 = Ball(50, 80, 5, -pi/4);
+%ball3 = Ball(50, 50, 5, 2*pi/3);
 hold on
-
+array = [ball1 ball2];
 for i=1:100
-    if (ball1.in_play)
-        ball1 = moveBall(ball1);
-        ball1 = checkTableCollision(ball1);
-        plot(ball1.pos_x, ball1.pos_y, 'ro');
+    for j=1:length(array)
+       %if (array(i).in_play)
+           array(j) = moveBall(array(j));
+         %  array(i) = checkTableCollision(array(i));
+           array = checkCollision(array, j);
+           plot(array(j).pos_x, array(j).pos_y, 'ro');
+           hold on
+      % end
     end
-    if (ball2.in_play)
-        ball2 = moveBall(ball2);
-        ball2 = checkTableCollision(ball2);
-        plot(ball2.pos_x, ball2.pos_y, 'bo');
-    end
+    
 end
