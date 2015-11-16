7b0f863b6165d803662549788c33b78eb079026c
davpapp <david.papp@students.olin.edu>
2015-11-16 12:29:05 -0500

    wokring on checkTableCollision

--- moveBall.m
+++ moveBall.m
@@ -2,4 +2,5 @@ function ball = moveBall(ball)
     ball.pos_x = ball.pos_x + cos(ball.v_theta) * ball.v_d;
     ball.pos_y = ball.pos_y + sin(ball.v_theta) * ball.v_d;
     %ball.v_d = ball.v_d - 0.5*0.01*ball.radius^2*pi*1.28*ball.v_d^2 / ball.mass;
+    ball.v_d = mod(ball.v_d, 2 * pi);
 end
