3be97d849673466fe4cc98ce866492274abc6414
JCMathai <john.mathai1@students.olin.edu>
2015-11-16 11:57:03 -0500

    Updated angle it comes off of.

--- checkTableCollision.m
+++ checkTableCollision.m
@@ -19,11 +19,10 @@ function ball = checkTableCollision (ball)
         if (ball.pos_x > 46.5 && ball.pos_x < 53.5)
             ball.in_play = false;
         else
-            if (ball.v_theta < 180)
-                ball.v_theta = mod(ball.v_theta, 90);
-            else
-                ball.v_theta = -mod(ball.v_theta, 90);
-            end
+                ball.v_theta = 360 - 2*ball.v_theta;
+                ball.v_d = -1*ball.v_d;
+            
+           
         end
     end
  end
