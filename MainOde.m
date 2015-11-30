0b0f2abed1494a0df747d087ae28cda71ba4cfd2
davpapp <david.papp@students.olin.edu>
2015-11-30 11:58:05 -0500

    changed to vector math

--- MainOde.m
+++ MainOde.m
@@ -4,7 +4,8 @@ hold on
 
 ball1 = Ball(20, 40, 1, 2, 'r');
 ball2 = Ball(50, 60, 2, 1, 'b');
-balls = [ball1 ball2];
+ball3 = Ball(40, 30, 0, 1.5, 'g');
+balls = [ball1 ball2 ball3];
 ball_specs = 1:(length(balls) * 4);
 for i=1:length(balls)
     ball_specs(4*i - 3) = balls(i).pos_x;
@@ -14,7 +15,7 @@ for i=1:length(balls)
 end
 
 for i=1:10
-    %options = odeset(@checkForContact, 'Events');
+   % options = odeset(@checkForContact, 'Events');
     [X, Y] = ode45(@moveBallOde, [0 5], ball_specs, [], balls);
     for j=1:length(balls)
         plot(Y(:,4*j - 3), Y(:,4*j - 2));
---
+++ checkForContact.m
@@ -0,0 +1,8 @@
+function [value, isterminal, direction] = checkForContact(~, balls)
+    for i=length(balls)
+       x_diff = ballsx - 3.5; 
+    end
+    value = 
+    isterminal = 0;
+    direction = 0;
+end
