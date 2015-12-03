61ef28fe82e9fe5ad3f39684b00c20e2c1f76652
davpapp <david.papp@students.olin.edu>
2015-12-03 11:00:16 -0500

    Working on multi collisions

--- Main.m
+++ Main.m
@@ -1,8 +1,8 @@
 %main.m
 clf
-ball1 = Ball(20, 40, 1, 0, 'ro');
-ball2 = Ball(40, 20, 0.1, 1, 'bo');
-ball3 = Ball(50, 50, 1.5, 2*pi/3, 'go');
+ball1 = Ball(30, 53, 0.7, 0, 'ro');
+ball2 = Ball(50, 50, 0, 0, 'bo');
+ball3 = Ball(50, 70, 0, 0, 'go');
 ball4 = Ball(61, 75, 2.5, -pi/4, 'ro');
 ball5 = Ball(91, 16, 1.2, 15/7, 'ro');
 ball6 = Ball(53, 39, 0.7, 6*pi/3, 'ro');
@@ -14,11 +14,11 @@ ball11 = Ball(7, 5, 1, 74, 'ro');
 ball12 = Ball(50, 91, 1, 13, 'ro');
 
 hold on
-array = [ball1 ball2]% ball3 ball4]% ball5 ball6 ball7 ball8 ball9 ball10 ball11 ball12];
+array = [ball1 ball2 ball3];% ball4]% ball5 ball6 ball7 ball8 ball9 ball10 ball11 ball12];
 
 axis([0 100 0 100]);
 
-for i=1:900
+for i=1:200
     array = checkCollision(array);
     for j=1:length(array)
        if (array(j).in_play)
@@ -28,5 +28,4 @@ for i=1:900
            drawnow
        end
     end
-   % array = checkCollision(array);
 end
